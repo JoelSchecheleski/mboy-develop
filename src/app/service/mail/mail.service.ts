bdd2cb41e4e47b626ccca16cf0d72e2a7449ea6e
joelschecheleski <joel.jsp@gmail.com>
2019-10-23 16:06:48 -0300

    Ajustado grid

--- src/app/service/mail/mail.service.ts
+++ src/app/service/mail/mail.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@angular/core';
-// import { Mail} from "../../inbox/inbox/mail.model";
-// import { demoMails} from "../../inbox/inbox/mails.demo";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ResourceService} from '../../ResourceService';
+import {Observable} from 'rxjs';
+import {Config} from '../../app-config';
+
+import {EmailModel} from '../../modules/modelos/emailModel';
 
 @Injectable()
-export class MailService {
+export class MailService extends ResourceService<EmailModel> {
+    private dados: any;
+    public client_http: HttpClient;
 
-  // getMessages(): Promise<Mail[]> {
-  //   return Promise.resolve(demoMails);
-  // }
+    constructor(httpClient: HttpClient) {
+        super(
+            httpClient,
+            'email/simple');
+        this.client_http = httpClient;
+    }
+
+    public emailData(file: any): Observable<any> {
+        this.dados = {
+            to: file ? file.security : '',
+            subject: file ? file.subject : '',
+            content: file ? file.content : '',
+            multipart: file ? file.multipart : '',
+            html: file ? file.html : '',
+        };
+        return this.dados;
+    }
 }
+
+
+
